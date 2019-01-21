1f0ef87ae88a9c46e4f5be0d34508477af48e1dc
jdenozi <denozi.j@gmail.com>
2019-01-21 03:37:38 +0100

    Commit

--- main.py
+++ main.py
@@ -2,22 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from appartment_group_handler import validatePrice
+from selenium import webdriver
 
 def args():
     parser=argparse.ArgumentParser(description="Retrouve toutes les annonces correspondant aux critères mis en paramètres")
 
     parser.add_argument("-n","--number", help="Donne le carré du nombre", type=int)
-    args=parser.parse_args()
 
-    parser.add_argument("--gf", "--facebook_group", help="Groupe dans lequelle vous voulez chercher", required=False, type=str)
+    parser.add_argument("--gf","--facebookGroup", help="Groupe dans lequelle vous voulez chercher", type=str)
 
-    parser.add_argument("--pr", "-price_range", help="Intervalle de prix pou filtrer les annonces ->exemple 450 & 550 ", required=False, type=str)
+    parser.add_argument("--pr", "--priceRange", help="Intervalle de prix pou filtrer les annonces ->exemple 450 & 550 ", required=False, type=str)
 
-    parser.add_argument("-l", "-localisation", help="Filtre sur les lieu des annonces", required=False, type=str)
+    parser.add_argument("-l", "--localisation", help="Filtre sur les lieu des annonces", required=False, type=str)
     
     return parser.parse_args()
 
 if __name__ == '__main__':
     arguments=args()   
-    print(arguments.number**2)
+    if arguments.number is not None:
+        print(arguments.number**2)
+    if arguments.pr is not None:
+        validatePrice(arguments.pr)
 
