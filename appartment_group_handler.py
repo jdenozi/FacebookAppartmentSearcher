19141f33c42aa938f85c37aecb26fc142b3aab11
jdenozi <denozi.j@gmail.com>
2019-01-21 03:37:50 +0100

    Commit

--- appartment_group_handler.py
+++ appartment_group_handler.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 # *-* coding: utf8 *-*
 import re
-def validatePrice(price):# type str
-    regex="\w*"
-    list_price=re.findall(regex,price)
-    print(list_price)
-    
 
+
+def validatePrice(price):# type str
+    regex="[0-9]*"
+    list_price=re.findall(regex,price)
+    list_price=[i for i in list_price if i!=""]
+    print(len(list_price))
+    if len(list_price)==2:
+        print(list_price)
+        return list_price
+    else:
+        return 
+
+class groupFacebook():
+    def __init__(self, groupFacebook, price, localisation):
+        self.groupFacebook=groupFacebook
+        self.price=price
+        self.localisation=localisation
 
